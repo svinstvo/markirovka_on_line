8d5b511bb224e863e7967287ee93edfecd9f74b0
byrikin <byrikin@klever.ru>
2021-12-21 11:03:00 +0300

    multiple socket server

--- plc_connector.py
+++ plc_connector.py
@@ -7,11 +7,12 @@ async def handle_km(reader, writer):
     while True:
         data = await reader.read(100)
         message = data.decode()
-        print(f"->{message}<-")
+        print(f"->{message}<- receive")
 
-        #Если прилетает 1 символ значит это тест канала
+        # Если прилетает 1 символ значит это тест канала
         if len(message) == 1:
             writer.write(data)
+            print(f"->{data}<- send from ping")
             await writer.drain()
             continue
 
@@ -27,7 +28,7 @@ async def handle_km(reader, writer):
                 resp_text = await resp.text()
 
         addr = writer.get_extra_info('peername')
-        print(f"-->{resp_text}<--")
+        print(f"-->{resp_text}<-- send from add_km")
         if resp_text == "ok":
             writer.write(b'0')
         else:
@@ -39,13 +40,76 @@ async def handle_km(reader, writer):
     await writer.wait_closed()
     print(writer.is_closing())
 
+#Метки приходят в ответ 1 или 0
+async def handle_port2000(reader, writer):
+    while True:
+        try:
+            data = await reader.read(100)
+            #print(f"->{data}<- raw receive om 2000")
+            message = data.decode()
+            print(f"->{message}<- receive on 2000")
+            writer.write(b"1 ")
+            await writer.drain()
+        except Exception as e:
+            print(e)
+            writer.write(b'1')
+            print("send 1 on 2000 port")
+            await writer.drain()
+
+#пинг контроллера что слышим то и передаем
+async def handle_port2001(reader, writer):
+    while True:
+        try:
+            data = await reader.read(100)
+            #print(f"->{data}<- raw receive om 2001")
+            message = data.decode()
+            print(f"->{message}<- receive on 2001")
+            writer.write(data)
+            await writer.drain()
+        except Exception as e:
+            print(e)
+            writer.write(b'1 ')
+            print("send 1 on 2001 port")
+            await writer.drain()
+
+#принимаем два инта, в ответ настройки
+async def handle_port2002(reader, writer):
+    while True:
+        try:
+            data = await reader.read(100)
+            #print(f"->{data}<- raw receive om 2002")
+            message = data.decode()
+            print(f"->{message}<- receive on 2002")
+            writer.write(data)
+            await writer.drain()
+        except Exception as e:
+            print(e)
+
+
+async def handle_port2004(reader, writer):
+    while True:
+        try:
+            data = await reader.read(100)
+            print(f"->{data}<- raw receive on 2004")
+            message = data.decode()
+            print(f"->{message}<- receive on 2004")
+            writer.write(data)
+            await writer.drain()
+        except Exception as e:
+            print(e)
+
 
 async def start_servers(app):
     loop = asyncio.get_event_loop()
-    server_km = await asyncio.start_server(handle_km, '0.0.0.0', 2000, loop=loop)
-    addr = server_km.sockets[0].getsockname()
-    print(f'server for data from scanners run  on {addr}')
-    await asyncio.start_server(handle_km, '0.0.0.0', 2001, loop=loop, start_serving=True)
+    # server_km = await asyncio.start_server(handle_km, '0.0.0.0', 2000, loop=loop)
+    # addr = server_km.sockets[0].getsockname()
+    # print(f'server for data from scanners run  on {addr}')
+    # await asyncio.start_server(handle_km, '0.0.0.0', 2001, loop=loop, start_serving=True)
+
+    await asyncio.start_server(handle_port2000, '0.0.0.0', 2000, loop=loop, start_serving=True)
+    await asyncio.start_server(handle_port2001, '0.0.0.0', 2001, loop=loop, start_serving=True)
+    await asyncio.start_server(handle_port2002, '0.0.0.0', 2002, loop=loop, start_serving=True)
+    await asyncio.start_server(handle_port2004, '0.0.0.0', 2004, loop=loop, start_serving=True)
 
     # async with server:
     #    await server.serve_forever()
