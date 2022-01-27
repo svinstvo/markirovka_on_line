aebe1a52c93da96ecfe5b45274e17f80cb42620e
byrikin <byrikin@klever.ru>
2022-01-27 11:09:00 +0300

    wrong gtin cause message

--- km.py
+++ km.py
@@ -12,6 +12,7 @@ async def km_add(request):
     gtin = request.app["current_gtin"]
     batch_date = request.app['current_batch_date']
     redis_timeout=request.app['redis_timeout']
+
     if gtin == "":
         asyncio.create_task(web_interface.show_error("Не выбран продукт"))
         return web.Response(text="no selected product")
@@ -40,36 +41,45 @@ async def km_add(request):
             print(e)
             print('ne razobrat')
             asyncio.create_task(work_with_db.save_logs(app=request.app, message=e))
+            getting_gtin = gtin
+            getting_tail = ""
+            getting_crypto_tail = ""
 
-        if request.app["enable_unique_check"] == 1:
-            try:
-                inserted_rows = await asyncio.wait_for(redis.sadd(getting_gtin, getting_tail), timeout=redis_timeout)
-                # inserted_rows = await redis.sadd(getting_gtin, getting_tail)
-            except Exception as e:
-                print(e)
-                inserted_rows = 1
-                asyncio.create_task(work_with_db.save_logs(app=request.app,message=e))
-        else:
-            inserted_rows = 1
-
-        # print(inserted_rows)
-        # print(km)
-        if inserted_rows == 1:
-            # print('add')
-            status = "verified"
-            request.app['counters']['good_codes'] += 1
-            response_text = "ok"
-            request.app['last_10_codes'].append(raw_km)
-            request.app['status']['state'] = 0
-            request.app['status']['message'] = 'Все хорошо'
-        else:
-            # print(request.app['counters'])
+        if getting_gtin!=gtin:
             request.app['counters']['duplicates_codes'] += 1
-            status = "duplicate"
+            status = "wrong_product"
             response_text = "duplicate"
-            request.app['last_10_codes'].append("Повторный " + raw_km)
+            request.app['last_10_codes'].append("Не тот продукт " + raw_km)
             request.app['status']['state'] = 1
-            request.app['status']['message'] = "Дублирование кода"
+            request.app['status']['message'] = "Не тот продукт"
+
+        else:
+            if request.app["enable_unique_check"] == 1:
+                try:
+                    inserted_rows = await asyncio.wait_for(redis.sadd(getting_gtin, getting_tail), timeout=redis_timeout)
+                    # inserted_rows = await redis.sadd(getting_gtin, getting_tail)
+                except Exception as e:
+                    print(e)
+                    inserted_rows = 1
+                    asyncio.create_task(work_with_db.save_logs(app=request.app,message=e))
+            else:
+                inserted_rows = 1
+            if inserted_rows == 1:
+                # print('add')
+                status = "verified"
+                request.app['counters']['good_codes'] += 1
+                response_text = "ok"
+                request.app['last_10_codes'].append(raw_km)
+                request.app['status']['state'] = 0
+                request.app['status']['message'] = 'Все хорошо'
+            else:
+                # print(request.app['counters'])
+                request.app['counters']['duplicates_codes'] += 1
+                status = "duplicate"
+                response_text = "duplicate"
+                request.app['last_10_codes'].append("Повторный " + raw_km)
+                request.app['status']['state'] = 1
+                request.app['status']['message'] = "Дублирование кода"
 
     # print(request.app['status'])
     request.app['counters']['total_codes'] += 1
